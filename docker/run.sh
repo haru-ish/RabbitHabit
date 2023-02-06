11ab38fbce45580e92a792cc4c7a851eb93fd4c9
haru-ish <haru-ish@j0d.de>
2023-02-06 09:10:47 +0100

    Delete an unnecessary file in docker
    - Delete run.sh in docker

--- docker/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm --name=rabbit-habit -e POSTGRES_PASSWORD=postgres -e FIREBASE_ID=rabbit-tracker-51700 -v rabbitdb:/var/lib/postgresql/data -p 4278:4278 rabbit-habit
